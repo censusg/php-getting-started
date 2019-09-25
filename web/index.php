c832c6f86ad92e77ee3046a6ba83208cefca639f
censusg <55761898+censusg@users.noreply.github.com>
2019-09-25 17:58:50 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,25 +1,42 @@
 <?php
+// Reads the variables sent via POST from our gateway
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
 
-require('../vendor/autoload.php');
+if ($text == "") {
+    // This is the first request. Note how we start the response with CON
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
 
-$app = new Silex\Application();
-$app['debug'] = true;
+} else if ($text == "1") {
+    // Business logic for first level response
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
+    $response .= "2. Account balance";
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+} else if ($text == "2") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+} else if($text == "1*1") { 
+    // This is a second level response where the user selected 1 in the first instance
+    $accountNumber  = "ACC1001";
 
-// Our web handlers
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your account number is ".$accountNumber;
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+} else if ( $text == "1*2" ) {
+    // This is a second level response where the user selected 1 in the first instance
+    $balance  = "KES 10,000";
 
-$app->run();
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your balance is ".$balance;
+}
+
+// Echo the response back to the API
+header('Content-type: text/plain');
+echo $response;
